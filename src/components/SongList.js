cc6283c14eb1a7193678c20565bc7d1e73eb5d2a
Jose Pluymaekers <jose@mcb-jp.home>
2021-05-07 16:23:47 +0200

    nu dan

---
+++ src/components/SongList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./styles/styles.css";
+
+const SongList = (props) => {
+  return (
+    <div style={{ maxWidth: "70%", margin: "0 auto" }} id="songTable">
+      <table style={{ width: "100%", marginTop: "5em" }}>
+        <tr>
+          <th>Title</th>
+          <th>Artist</th>
+          <th>Genre</th>
+          <th>Rating</th>
+        </tr>
+        {props.songs.map((song, index) => {
+          return (
+            <tr key={index} value={song.id}>
+              <td>{song.title}</td>
+              <td>{song.artist}</td>
+              <td>{song.genre}</td>
+              <td>{song.rating}</td>
+              <button onClick></button>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
+};
+export default SongList;
---
+++ src/components/SongOverview.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./styles/styles.css";
+
+import SongForm from "./SongForm";
+import SongList from "./SongList";
+
+function SongOverview() {
+  const [songs, setSongs] = useState([]);
+  const addSong = (song) => {
+    const newSongList = [...songs, song];
+    setSongs(newSongList);
+  };
+
+//   function deleteSong(song) {
+//     const newSongList = songs.filter((item) => song.id !== song.id);
+//     setSongs(newSongList);
+//   }
+
+  return (
+    <div>
+      <header className="header">Mijn playlist</header>
+      <div>
+        <SongForm addSong={addSong} />
+        <SongList songs={songs} />
+      </div>
+    </div>
+  );
+}
+export default SongOverview;
